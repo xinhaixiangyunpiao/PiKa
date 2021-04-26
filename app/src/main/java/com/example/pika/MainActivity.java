78aa6e5c4787f5f1ac925729fa1d577345751d0a
xinhaixiangyunpiao <15600761271@163.com>
2021-04-27 01:03:03 +0800

    add image select.

--- app/src/main/java/com/example/pika/MainActivity.java
+++ app/src/main/java/com/example/pika/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.pika;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.wildma.pictureselector.PictureBean;
+import com.wildma.pictureselector.PictureSelector;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView image;
+    private Button select;
+    private Button transport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image = findViewById(R.id.image);
+        select = findViewById(R.id.select_button);
+        transport = findViewById(R.id.transport_button);
+
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PictureSelector
+                        .create(MainActivity.this, PictureSelector.SELECT_REQUEST_CODE)
+                        .selectPicture(true, 200, 200, 1, 1);
+            }
+        });
+
+        transport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        /*结果回调*/
+        if (requestCode == PictureSelector.SELECT_REQUEST_CODE) {
+            if (data != null) {
+                PictureBean pictureBean = data.getParcelableExtra(PictureSelector.PICTURE_RESULT);
+                if (pictureBean.isCut()) {
+                    image.setImageBitmap(BitmapFactory.decodeFile(pictureBean.getPath()));
+                } else {
+                    image.setImageURI(pictureBean.getUri());
+                }
+            }
+        }
     }
 }
