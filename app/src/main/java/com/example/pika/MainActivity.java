710bdbfedd4cea59f9e274d774072888ae301d22
xinhaixiangyunpiao <15600761271@163.com>
2021-04-28 00:07:53 +0800

    add ble connect.

--- app/src/main/java/com/example/pika/MainActivity.java
+++ app/src/main/java/com/example/pika/MainActivity.java
@@ -2,20 +2,46 @@ package com.example.pika;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothClass;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.wildma.pictureselector.PictureBean;
 import com.wildma.pictureselector.PictureSelector;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_ENABLE_LOCATION = 1;
     private ImageView image;
     private Button select;
     private Button transport;
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothLeScanner bluetoothLeScanner;
+    private final static int REQUEST_ENABLE_BT = 1;
+    private boolean mScanning;
+    private Handler handler = new Handler();
+    private static final long SCAN_PERIOD = 10000;
+    private List<BluetoothDevice> devices = new ArrayList<BluetoothDevice>();
+    private BluetoothDevice nrf52840;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +66,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        // 获取蓝牙适配器
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
+
+        // 确认当前设备的蓝牙是否可用,
+        // 如果不可用, 弹出一个对话框, 请求打开设备的蓝牙模块
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        // 检查权限
+        checkPermission();
+
+        // 开始扫描ble设备
+        scanLeDevice(true);
+        Log.println(Log.INFO,"her","hahahahah");
     }
 
     @Override
@@ -57,4 +102,50 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    private void checkPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+        if (checkSelfPermission(permissions[0]) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(permissions, REQUEST_ENABLE_LOCATION);
+        }
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    bluetoothLeScanner.stopScan(scanCallback);
+                }
+            }, SCAN_PERIOD);
+            mScanning = true;
+            bluetoothLeScanner.startScan(scanCallback);
+            Log.println(Log.INFO,"info","lalala");
+        } else {
+            mScanning = false;
+            bluetoothLeScanner.stopScan(scanCallback);
+        }
+    }
+
+    //startScan()回调函数
+    private ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult results) {
+            super.onScanResult(callbackType, results);
+            BluetoothDevice device = results.getDevice();
+            if (!devices.contains(device)) {  //判断是否已经添加
+                devices.add(device);//也可以添加devices.getName()到列表，这里省略
+                if (device.getAddress().equals("D0:4A:81:A5:A5:D3")) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "连接成功", Toast.LENGTH_SHORT);
+                    toast.show();
+                    nrf52840 = device;
+                    // 关闭ble设备扫描
+                    scanLeDevice(false);
+                }
+            }
+        };
+    };
+
 }
